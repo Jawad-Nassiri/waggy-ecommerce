02f632a6fd40ec95b1e8d2d5319af2fe0863d671
Jawad <jawadnasiri2222@gmail.com>
2026-09-10 17:52:17 +0200

    WIP: order status changes

--- waggy-backend/src/main/java/com/waggy/mapper/OrderMapper.java
+++ waggy-backend/src/main/java/com/waggy/mapper/OrderMapper.java
@@ -55,7 +55,8 @@ public class OrderMapper {
                         .stream()
                         .map(item -> item.getPrice()
                                 .multiply(BigDecimal.valueOf(item.getQuantity())))
-                        .reduce(BigDecimal.ZERO, BigDecimal::add)
+                        .reduce(BigDecimal.ZERO, BigDecimal::add),
+                order.getStatus()
         );
     }
 
