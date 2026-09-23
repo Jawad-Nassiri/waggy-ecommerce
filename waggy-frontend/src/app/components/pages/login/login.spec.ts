4a400c94e210a86e6e3b6bfe83445d6f5c0665e5
Jawad <jawadnasiri2222@gmail.com>
2026-09-23 14:35:28 +0200

    add login pages

--- waggy-frontend/src/app/components/pages/login/login.spec.ts
+++ waggy-frontend/src/app/components/pages/login/login.spec.ts
@@ -1,15 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideHttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 import { Login } from './login';
+import { of, throwError } from 'rxjs';
 
 describe('Login', () => {
   let component: Login;
   let fixture: ComponentFixture<Login>;
 
+  const authServiceMock = {
+    loadCurrentUser: vi.fn(),
+    login: vi.fn(),
+  };
+
+  const routerMock = {
+    navigate: vi.fn(),
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Login],
-    }).compileComponents();
+      providers: [
+        { provide: AuthService, useValue: authServiceMock },
+        { provide: Router, useValue: routerMock },
+      ],
+    })
+      .overrideComponent(Login, {
+        set: { template: '' },
+      })
+      .compileComponents();
 
     fixture = TestBed.createComponent(Login);
     component = fixture.componentInstance;
@@ -18,5 +38,77 @@ describe('Login', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should validate email', () => {
+    const input = document.createElement('input');
+
+    component.validateEmail('jawad@mail.com', input);
+
+    expect(component.emailValid).toBe(true);
+  });
+
+  it('should invalidate email', () => {
+    const input = document.createElement('input');
+
+    component.validateEmail('wrong-email', input);
+
+    expect(component.emailValid).toBe(false);
+  });
+
+  it('should validate password', () => {
+    const input = document.createElement('input');
+
+    component.validatePassword('Password1!', input);
+
+    expect(component.passwordValid).toBe(true);
+  });
+
+  it('should invalidate password', () => {
+    const input = document.createElement('input');
+
+    component.validatePassword('123', input);
+
+    expect(component.passwordValid).toBe(false);
+  });
+
+  it('should login successfully', () => {
+    authServiceMock.login.mockReturnValue(of({}));
+
+    component.emailValid = true;
+    component.passwordValid = true;
+
+    const emailInput = document.createElement('input');
+    const passwordInput = document.createElement('input');
+
+    emailInput.value = 'jawad@mail.com';
+    passwordInput.value = 'Password1!';
+
+    component.login(emailInput, passwordInput);
+
+    expect(authServiceMock.login).toHaveBeenCalledWith('jawad@mail.com', 'Password1!');
+    expect(routerMock.navigate).toHaveBeenCalledWith(['/'], {
+      state: { toastMessage: 'Login successful!' },
+    });
+  });
+
+  it('should show error when login fails', () => {
+    authServiceMock.login.mockReturnValue(throwError(() => ({ error: 'Wrong password' })));
+
+    component.emailValid = true;
+    component.passwordValid = true;
+
+    const emailInput = document.createElement('input');
+    const passwordInput = document.createElement('input');
+
+    emailInput.value = 'jawad@mail.com';
+    passwordInput.value = 'WrongPassword1!';
+
+    component.login(emailInput, passwordInput);
+
+    expect(component.toastTitle).toBe('Error');
+    expect(component.toastMessage).toBe('Wrong password');
+    expect(component.toastType).toBe('error');
+    expect(component.showToast()).toBe(true);
   });
 });
--- waggy-frontend/src/app/components/pages/login/login.ts
+++ waggy-frontend/src/app/components/pages/login/login.ts
@@ -1,9 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Banner } from '../../banner/banner';
+import { Gallery } from '../../gallery/gallery';
+import { galleryImages } from '../../../data/gallery-images';
+import { AuthService } from '../../../services/auth.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Toast } from '../../toast/toast';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [RouterLink, Banner, Gallery, FormsModule, Toast],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {}
+
+  galleryImages = galleryImages;
+
+  passwordValid = false;
+  emailValid = false;
+  showToast = signal(false);
+  toastTitle = '';
+  toastMessage = '';
+  toastType = '';
+
+  changeEyeIconStatus(event: Event, input: HTMLInputElement): void {
+    const icon = event.target as HTMLElement;
+
+    icon.classList.toggle('fa-eye-slash');
+    icon.classList.toggle('fa-eye');
+
+    input.type = input.type === 'text' ? 'password' : 'text';
+  }
+
+  validateEmail(email: string, input: HTMLInputElement): void {
+    this.emailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+
+    input.style.borderColor = email.length === 0 ? '' : this.emailValid ? 'green' : 'red';
+  }
+
+  validatePassword(password: string, input: HTMLInputElement): void {
+    this.passwordValid = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[^A-Za-z\d]).{6,}$/.test(password);
+
+    input.style.borderColor = password.length === 0 ? '' : this.passwordValid ? 'green' : 'red';
+  }
+
+  login(emailInput: HTMLInputElement, passwordInput: HTMLInputElement): void {
+    const email = emailInput.value;
+    const password = passwordInput.value;
+
+    if (!email || !password || !this.emailValid || !this.passwordValid) {
+      return;
+    }
+
+    this.authService.login(email, password).subscribe({
+      next: (user) => {
+        console.log('current user', user)
+        emailInput.value = '';
+        passwordInput.value = '';
+
+        this.passwordValid = false;
+        this.emailValid = false;
+
+        this.router.navigate(['/'], {
+          state: { toastMessage: 'Login successful!' },
+        });
+      },
+
+      error: (error) => {
+        this.toastTitle = 'Error';
+        this.toastMessage = error.error || 'Login failed.';
+        this.toastType = 'error';
+        this.showToast.set(true);
+      },
+    });
+  }
+}
