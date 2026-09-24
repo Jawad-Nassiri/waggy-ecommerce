ddcc7b2e35690794c6dfc618957e71e636d4c26d
Jawad <jawadnasiri2222@gmail.com>
2026-09-24 16:41:08 +0200

    fix active navigation links for navbar

--- waggy-frontend/src/app/components/header/header.ts
+++ waggy-frontend/src/app/components/header/header.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
